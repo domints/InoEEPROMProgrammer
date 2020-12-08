b77e265dd53a5166235f23ceed855eb1744806fa
Dominik Szymański <domints@gmail.com>
2020-12-08 18:10:46 +0100

    Add support for multibyte address (>16K EEPROMs)

--- I2CEEPROM.cs
+++ I2CEEPROM.cs
@@ -7,9 +7,11 @@ namespace InoEEPROMProgrammer
 {
     public class I2CEEPROM
     {
+        private const int INTERFACE_SUPPORTED_PAGE_SIZE = 16;
         private readonly SerialPortWrapper _port;
         private readonly byte _deviceAddress;
         private readonly int _blockSize;
+        private readonly int _pageSize;
         private readonly EEPROMDefinition _definition;
 
         public I2CEEPROM(string portName, byte deviceAddress, EEPROMDefinition definition)
@@ -23,6 +25,7 @@ namespace InoEEPROMProgrammer
             _deviceAddress = deviceAddress;
 
             _definition = definition;
+            _pageSize = Math.Min(INTERFACE_SUPPORTED_PAGE_SIZE, _definition.PageSize);
             _blockSize = (definition.MemorySize / definition.WordSize) / definition.BlockCount;
         }
 
@@ -41,10 +44,10 @@ namespace InoEEPROMProgrammer
         private byte[] ReadBlock(int blockNumber, int address, int count)
         {
             List<byte> output = new List<byte>();
-            var ops = OperationSequenceGenerator.Generate(_definition.PageSize, address, count);
+            var ops = OperationSequenceGenerator.Generate(_pageSize, address, count);
             foreach (var op in ops)
             {
-                var opAddr = (op.AbsoluteMemBlockNumber * _definition.PageSize) + op.BlockOperationStart;
+                var opAddr = (op.AbsoluteMemBlockNumber * _pageSize) + op.BlockOperationStart;
                 output.AddRange(ReadPage((byte)(_deviceAddress + blockNumber), opAddr, op.OperationBlockSize));
             }
 
@@ -53,9 +56,10 @@ namespace InoEEPROMProgrammer
 
         private byte[] ReadPage(byte devAddr, int address, int count)
         {
-            byte[] initBuffer = new[] { I2CCommands.SEND, devAddr, (byte)1, (byte)0, (byte)address };
+            List<byte> initBuffer = new List<byte> { I2CCommands.SEND, devAddr, (byte)_definition.AddressSize, 0 };
+            initBuffer.AddRange(GetDeviceSpecificAddressBytes(address));
             byte[] readCmd = new[] { I2CCommands.READ, devAddr, count.GetNthByte(0), count.GetNthByte(1) };
-            _port.WriteBytes(initBuffer);
+            _port.WriteBytes(initBuffer.ToArray());
             _port.WriteBytes(readCmd);
             int receiveCount = _port.ReadByte() | (_port.ReadByte() << 8);
             return _port.ReadBytes(receiveCount);
@@ -65,7 +69,7 @@ namespace InoEEPROMProgrammer
         {
             if(data.Length + address > _definition.MemorySize / 8)
                 throw new ApplicationException("Data too long to write at given address!");
-                
+
             var ops = OperationSequenceGenerator.Generate(_blockSize, address, data.Length);
             foreach (var op in ops)
             {
@@ -76,30 +80,42 @@ namespace InoEEPROMProgrammer
 
         private void WriteBlock(int blockNumber, int address, byte[] data)
         {
-            var ops = OperationSequenceGenerator.Generate(_definition.PageSize, address, data.Length);
+            var ops = OperationSequenceGenerator.Generate(_pageSize, address, data.Length);
             foreach (var op in ops)
             {
-                var opAddr = (op.AbsoluteMemBlockNumber * _definition.PageSize) + op.BlockOperationStart;
-                var pageData = data.Skip(op.DataOperationStart).Take(_definition.PageSize).ToArray();
+                var opAddr = (op.AbsoluteMemBlockNumber * _pageSize) + op.BlockOperationStart;
+                var pageData = data.Skip(op.DataOperationStart).Take(_pageSize).ToArray();
                 WritePage((byte)(_deviceAddress + blockNumber), opAddr, pageData);
             }
         }
 
         private void WritePage(byte devAddress, int address, byte[] data)
         {
-            var msgLength = data.Length + 1;
+            var msgLength = data.Length + _definition.AddressSize;
             List<byte> buffer = new List<byte>
             {
                 I2CCommands.SEND,
                 devAddress,
                 msgLength.GetNthByte(0),
                 msgLength.GetNthByte(1),
-                (byte)address
             };
+
+            buffer.AddRange(GetDeviceSpecificAddressBytes(address));
 
             buffer.AddRange(data);
 
             _port.WriteBytes(buffer.ToArray());
+        }
+
+        private IEnumerable<byte> GetDeviceSpecificAddressBytes(int address)
+        {
+            List<byte> result = new List<byte>();
+            for (int i = 0; i < _definition.AddressSize; i++)
+            {
+                result.Add(address.GetNthByte(_definition.AddressSize - 1 - i));
+            }
+
+            return result;
         }
     }
 }
